7e752985edb701d383dc3dfc4f6c2e8a5f95014d
madelynarsenault <madelyn.arsenault@gmail.com>
2020-05-25 20:53:14 -0600

    endpoints working lets goooo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Patient from './Patient'
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>DCG Patient Test!</h1>
-    <Patient/>
+      <Patient />
+  
 
     </div>
   );
--- src/Patient.js
+++ src/Patient.js
@@ -1,14 +1,46 @@
-import React from 'react'; 
+import React from 'react';
 import axios from 'axios';
 
-export default class Patient extends React.Component{
-    constructor(){
+export default class Patient extends React.Component {
+    constructor() {
         super();
-
-        axios.
-            get(`http://cqm-sandbox.alphora.com/cqf-ruler-r4/fhir/Patient/denom-EXM104-FHIR4`)
-
-
+        this.state = {
+            patient: {
+                id: null,
+                name: {
+                    family: null,
+                    given: null,
+                },
+                gender: null,
+                birthDate: null,
+                race: null,
+                ethnicity: null
+            }
+        }
+    }
+    
+    componentDidMount() {
+        axios.get(`http://cqm-sandbox.alphora.com/cqf-ruler-r4/fhir/Patient/denom-EXM104-FHIR4`)
+            .then(res => {
+                let patient_pulled = res.data;
+                patient_pulled.id = res.data.id;
+                patient_pulled.name.family = res.data.name[0].family;
+                patient_pulled.name.given = res.data.name[0].given[0];
+                patient_pulled.gender = res.data.gender;
+                patient_pulled.birthDate = res.data.birthDate;
+                this.setState({patient : patient_pulled});
+            });
+    }
+    
+    render() {
+        return (<div>
+                    <p>Patient First Name: {this.state.patient.name.given}</p>
+                    <p>Patient Last Name: {this.state.patient.name.family}</p> 
+                    <p>Patient Gender: {this.state.patient.gender}</p> 
+                    <p>Patient ID number: {this.state.patient.id}</p> 
+                    <p>Patient BirthDate: {this.state.patient.birthDate}</p> 
+                    </div>
+                );
     }
 }
-
+// ReactDOM.render(<Patient />, document.getElementById('patient'));
